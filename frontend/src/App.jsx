fd82fc123aac375823119deb0394b2f99bb35e02
ashik45690 <ashikedavakkath@gmail.com>
2026-08-18 15:22:23 +0530

    Fix forgot password route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
@@ -6,34 +7,83 @@ import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import Dashboard from "./pages/Dashboard";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import MyOrders from "./pages/Myorders";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import { Protuctroute } from "./components/ProtectRoute";
 import ForgotPassword from "./components/forgotpassword/ForgotPassword";
-import MyOrders from "./pages/Myorders";
 import OrderDetails from "./components/myOrders/OrderDetails";
-
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* Home */}
         <Route path="/" element={<Home />} />
-        <Route path="/user-login" element={<LoginPage />} />
-         <Route path="/user-Register" element={<RegisterPage/>} />
-         <Route path="/product-Page" element={<ProductPage/>}/>
-         <Route path="/Cart-items" element={<CartPage/>}/>
-         <Route path="/Check-out" element={<CheckoutPage/>}/>
-         <Route path="/Myorders" element={<MyOrders/>}/>
-         <Route path="/my-orders/:id" element={<OrderDetails />} />
-         <Route path="/Dashboard" element={
-          <Protuctroute>
-            <Dashboard/>
-          </Protuctroute>
-         }/>
-         
+
+        {/* Authentication */}
+        <Route
+          path="/user-login"
+          element={<LoginPage />}
+        />
+
+        <Route
+          path="/user-Register"
+          element={<RegisterPage />}
+        />
+
+        <Route
+          path="/forgot-password"
+          element={<ForgotPassword />}
+        />
+
+        {/* Products */}
+        <Route
+          path="/product-Page"
+          element={<ProductPage />}
+        />
+
+        {/* Cart */}
+        <Route
+          path="/Cart-items"
+          element={<CartPage />}
+        />
+
+        {/* Checkout */}
+        <Route
+          path="/Check-out"
+          element={<CheckoutPage />}
+        />
+
+        {/* Orders */}
+        <Route
+          path="/Myorders"
+          element={<MyOrders />}
+        />
+
+        <Route
+          path="/my-orders/:id"
+          element={<OrderDetails />}
+        />
+
+        {/* Dashboard */}
+        <Route
+          path="/Dashboard"
+          element={
+            <Protuctroute>
+              <Dashboard />
+            </Protuctroute>
+          }
+        />
+
       </Routes>
-       <ToastContainer />
+
+      {/* Toast Notifications */}
+      <ToastContainer />
+
     </BrowserRouter>
   );
 }
